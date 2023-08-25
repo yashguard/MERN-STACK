b4972e8b4e0903d2613174ce4128af2a9dfd942f
Yash Guard <yashguard>
2023-08-25 18:18:58 +0530

    Backend authentication has been completed

--- Backend/middleware/catchAsyncErrors.js
+++ Backend/middleware/catchAsyncErrors.js
@@ -1,5 +1,5 @@
 const catchAsyncError = (theFunction) => (req, res, next) => {
-  Promise.resolve(theFunction(req, res, next)).catch((err) => console.log(err));
+  Promise.resolve(theFunction(req, res, next)).catch(next);
 };
 
 module.exports = catchAsyncError;
--- Backend/middleware/error.js
+++ Backend/middleware/error.js
@@ -4,9 +4,26 @@ const errorFunction = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal Server Error";
 
-  // wrong mongodb id error
+  // Wrong mongodb id error
   if (err.name === "CastError") {
     const message = `Resource has not been founded. Invalid : ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
+  // Duplicate key error
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // Jwt error
+  if (err.name === "JsonWebTokenError") {
+    const message = `Invalid token has been entered, try again`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // Jwt expire error
+  if (err.name === "TokenExpiresError") {
+    const message = `Invalid token has been expired, try again`;
     err = new ErrorHandler(message, 400);
   }
 
