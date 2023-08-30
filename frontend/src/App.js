75d065831da3baa4df2897149e790399991e40db
Yash Guard <yashguard>
2023-08-30 12:01:03 +0530

    Header done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
 import "./App.css";
 import Header from "./Components/Layout/Header.jsx";
+import WebFont from "webfontloader";
 
 function App() {
+  useEffect(() => {
+    WebFont.load({
+      google: { families: ["Roboto", "Droid Sans", "Chilanka"] },
+    });
+  }, []);
   return (
     <>
       <Header />
